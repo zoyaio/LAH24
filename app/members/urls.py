aef71ff065076e50a9d2cfd633a4d0ab9e993af5
zoya <zdzhnou@gmail.com>
2024-04-07 04:11:05 -0700

    using mongo @ html!!! (only for events so far)

--- app/members/urls.py
+++ app/members/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('landing/', views.send_dictionary, name="landing"),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('events/<int:id>', views.eureka2, name="main")
+
 ]
